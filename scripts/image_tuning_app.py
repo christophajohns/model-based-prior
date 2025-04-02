275bb08a0191a13dbde56a6763578d31640293ec
Christoph A. Johns <christophjohns@gmail.com>
2025-04-02 11:14:35 +0200

    Add optional modification of original image before tuning

--- scripts/image_tuning_app.py
+++ scripts/image_tuning_app.py
@@ -1,9 +1,9 @@
 import os
 import logging
 import torch
+from dotenv import load_dotenv
 import matplotlib.pyplot as plt
 from torchvision.io import read_image
-from dotenv import load_dotenv
 from torchvision.transforms.functional import resize
 from modelbasedprior.prior import ModelBasedPrior
 from modelbasedprior.objectives import HumanEvaluatorObjective
@@ -23,27 +23,27 @@ load_dotenv()
 # Configuration
 SMOKE_TEST = True  # Set to True to use a synthetic function instead of an actual human evaluator
 ORIGINAL_IMAGE_PATH = os.getenv("ORIGINAL_IMAGE_PATH")
-OPTIMAL_CONFIGURATION = None # (0.8, 1.2, 1.2, 0.1)  # brightness, contrast, saturation, hue OR None
+ORIGINAL_MODIFICATION = (1., 1., 1., -0.)
+OPTIMAL_CONFIGURATION = None  # (0.8, 1.2, 1.2, 0.1)  # brightness, contrast, saturation, hue OR None
 SEED = 23489
 NUM_TRIALS = 5
 NUM_INITIAL_SAMPLES = 4
 IAL_K_LEVELS = 6  # Image Aesthetics Loss K-levels (original: 8; smaller is faster)
 IAL_ITERATIONS = 3  # Image Aesthetics Loss Domain Transform Filter Iterations (original: 5; smaller is faster)
+DOWNSAMPLING_SIZE = 64
 
 
 # Helper functions
 def compute_best_X_y(X: torch.Tensor, y: torch.Tensor):
-    # Compute cumulative maximum of y
     best_y, best_indices = torch.cummax(y, dim=0)
-    
-    # Select corresponding best X values
     best_X = X[best_indices.squeeze()]
-    
     return best_X, best_y
 
-def plot_image_similarity(X: torch.Tensor, original_image: torch.Tensor, target_image: torch.Tensor):
-    candidate_images = generate_image(X.unsqueeze(0), original_image).squeeze()
-    show_images(make_grid([original_image, target_image, *candidate_images]))
+
+def plot_image_similarity(X: torch.Tensor, original_image: torch.Tensor, modified_image: torch.Tensor, target_image: torch.Tensor):
+    candidate_images = generate_image(X.unsqueeze(0), modified_image).squeeze()
+    show_images(make_grid([original_image, modified_image, target_image, *candidate_images]))
+
 
 def plot_optimization_trace(
         ax: plt.Axes,
@@ -53,49 +53,39 @@ def plot_optimization_trace(
         ylabel: str = r"$\text{Observed } f_i(x)$",
         **kwargs,
     ) -> None:
-    """
-    Plot the observed function values over iterations.
-
-    Parameters:
-        ax (plt.Axes): The matplotlib axis to plot on.
-        y (torch.Tensor): A 1D tensor of shape (n,), containing the observed y values per iteration.
-        num_initial_samples (int): Number of initial random samples.
-    """
     iterations = torch.arange(len(y))
-
     ax.plot(iterations, y, marker='o', linestyle='-', alpha=0.9, **kwargs)
-
-    # Indicate initial samples
     ax.axvline(x=num_initial_samples - 0.5, color="darkgrey", linestyle="--", alpha=0.5)
-
     ax.set_xticks(iterations)
-
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
-
     ax.grid(True, linestyle="--", alpha=0.5)
 
 
 # Main script
 original_image = read_image(ORIGINAL_IMAGE_PATH)
-downsampled_original_image = resize(original_image, 64)
+downsampled_original_image = resize(original_image, DOWNSAMPLING_SIZE)
 
+# Modify the original image before optimization
+modified_image = generate_image(torch.tensor([[ORIGINAL_MODIFICATION]]), original_image).squeeze()
+downsampled_modified_image = resize(modified_image, DOWNSAMPLING_SIZE)
+
+# Determine prior function
 if OPTIMAL_CONFIGURATION is None:
-    prior_predict_func = ImageAestheticsLoss(original_image=downsampled_original_image, negate=True, k_levels=IAL_K_LEVELS, domain_transform_iterations=IAL_ITERATIONS)
+    prior_predict_func = ImageAestheticsLoss(original_image=downsampled_modified_image, negate=True, k_levels=IAL_K_LEVELS, domain_transform_iterations=IAL_ITERATIONS)
 else:
-    prior_predict_func = ImageSimilarityLoss(original_image=downsampled_original_image, optimizer=OPTIMAL_CONFIGURATION, weight_psnr=0.5, weight_ssim=0.5, negate=True)
+    prior_predict_func = ImageSimilarityLoss(original_image=downsampled_modified_image, optimizer=OPTIMAL_CONFIGURATION, weight_psnr=0.5, weight_ssim=0.5, negate=True)
 
 user_prior = ModelBasedPrior(bounds=prior_predict_func.bounds, predict_func=prior_predict_func, minimize=False, seed=SEED)
 
-logger = setup_logger(level=logging.INFO)  # or logging.DEBUG for more detailed output or logging.WARNING for less output
+logger = setup_logger(level=logging.INFO)
 
 if OPTIMAL_CONFIGURATION is None:
-    image_renderer = WebImageHumanEvaluatorRenderer(original_image)
+    image_renderer = WebImageHumanEvaluatorRenderer(modified_image)
 else:
-    image_renderer = WebImageHumanEvaluatorRenderer(original_image, optimal_transformation=OPTIMAL_CONFIGURATION)
+    image_renderer = WebImageHumanEvaluatorRenderer(modified_image, optimal_transformation=OPTIMAL_CONFIGURATION)
 
 if SMOKE_TEST:
     human_evaluator = prior_predict_func  # Mock
@@ -107,13 +97,11 @@ result_X, result_y, model = maximize(human_evaluator, user_prior=user_prior, num
 result_best_X, result_best_y = compute_best_X_y(result_X, result_y)
 
 if OPTIMAL_CONFIGURATION is None:
-    # Without target image
-    plot_image_similarity(result_X, original_image, original_image)
-    plot_image_similarity(result_best_X, original_image, original_image)
+    plot_image_similarity(result_X, original_image, modified_image, modified_image)
+    plot_image_similarity(result_best_X, original_image, modified_image, modified_image)
 else:
-    # For target image
-    plot_image_similarity(result_X, original_image, image_renderer._target_image)
-    plot_image_similarity(result_best_X, original_image, image_renderer._target_image)
+    plot_image_similarity(result_X, original_image, modified_image, image_renderer._target_image)
+    plot_image_similarity(result_best_X, original_image, modified_image, image_renderer._target_image)
 
 prior_predict_y = prior_predict_func(result_X)
 prior_predict_best_y = prior_predict_func(result_best_X)
